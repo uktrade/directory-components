252fe4b737c66a206ca8f80db6320797d641ccaf
richtier <rikatee@gmail.com>
2018-01-19 17:02:47 +0000

    Add absolute_static template tag

---
+++ tests/test_templatetags.py
@@ -0,0 +1,13 @@
+from django.template import Context, Template
+
+
+def test_static_absolute(rf):
+    template = Template(
+        '{% load static_absolute from directory_components_tags %}'
+        '{% static_absolute "directory_components/images/favicon.ico" %}'
+    )
+
+    context = Context({'request': rf.get('/')})
+    html = template.render(context)
+
+    assert html == 'http://testserver/directory_components/images/favicon.ico'
