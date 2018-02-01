c31d42f7036027bd564055193e2e4aac49622c8e
eadpearce <e.pearce@tutanota.com>
2018-02-01 14:31:41 +0000

    Updated tests. Removed yaml files

---
+++ directory_components/urls.py
@@ -0,0 +1,29 @@
+
+GREAT_HOME = 'https://great.gov.uk/'
+
+CUSTOM_PAGE = 'https://great.gov.uk/custom/'
+EXPORTING_NEW = 'https://great.gov.uk/new/'
+EXPORTING_OCCASIONAL = 'https://great.gov.uk/occasional/'
+EXPORTING_REGULAR = 'https://great.gov.uk/regular/'
+
+GUIDANCE_MARKET_RESEARCH = 'https://great.gov.uk/market-research/'
+GUIDANCE_CUSTOMER_INSIGHT = 'https://great.gov.uk/customer-insight/'
+GUIDANCE_FINANCE = 'https://great.gov.uk/finance/'
+GUIDANCE_BUSINESS_PLANNING = 'https://great.gov.uk/business-planning/'
+GUIDANCE_GETTING_PAID = 'https://great.gov.uk/getting-paid/'
+GUIDANCE_OPERATIONS_AND_COMPLIANCE = (
+    'https://great.gov.uk/operations-and-compliance/')
+
+SERVICES_FAB = 'https://find-a-buyer.export.great.gov.uk/'
+SERVICES_SOO = 'https://selling-online-overseas.export.great.gov.uk/'
+SERVICES_EXOPPS = 'https://opportunities.export.great.gov.uk/'
+SERVICES_GET_FINANCE = 'https://great.gov.uk/get-finance/'
+SERVICES_EVENTS = 'https://events.trade.gov.uk/'
+
+INFO_ABOUT = 'https://great.gov.uk/about/'
+INFO_CONTACT_US = 'https://contact-us.export.great.gov.uk/directory/'
+INFO_PRIVACY_AND_COOKIES = 'https://great.gov.uk/privacy-and-cookies/'
+INFO_TERMS_AND_CONDITIONS = 'https://great.gov.uk/terms-and-conditions/'
+INFO_DIT = (
+    'https://www.gov.uk/government/organisations/'
+    'department-for-international-trade/')
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,11 @@
 def pytest_configure():
     from django.conf import settings
     settings.configure(
+        ROOT_URLCONF='thing',
+        SSO_PROXY_LOGIN_URL='http://login.com',
+        SSO_PROXY_SIGNUP_URL='http://signup.com',
+        SSO_PROXY_LOGOUT_URL='http://logout.com',
+        SSO_PROFILE_URL='http://profile.com',
         INSTALLED_APPS=[
             'django.contrib.staticfiles',
             'directory_components',
