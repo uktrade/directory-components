8ff9c6ecf63c43eb9acd868fe05830d15b058658
eadpearce <e.pearce@tutanota.com>
2018-02-05 15:51:09 +0000

    Fixed indentation

--- directory_components/context_processors.py
+++ directory_components/context_processors.py
@@ -31,156 +31,159 @@ def analytics(request):
 
 def urls_processor(request):
     header_footer_links = {
-      "register": {
-        "title": "Register",
-        "id": "register-link"
+        "register": {
+            "title": "Register",
+            "id": "register-link"
         },
-      "signin": {
-        "title": "Sign in",
-        "id": "sign-in-link"
+        "signin": {
+            "title": "Sign in",
+            "id": "sign-in-link"
         },
-      "profile": {
-        "title": "Profile",
-        "id": "profile-link"
+        "profile": {
+            "title": "Profile",
+            "id": "profile-link"
         },
-      "signout": {
-        "title": "Sign out",
-        "id": "sign-out-link"
+        "signout": {
+            "title": "Sign out",
+            "id": "sign-out-link"
         },
-      "home": {
-        "title": "Home",
-        "id": "home-link",
-        "url": get_url("GREAT_HOME")
+        "home": {
+            "title": "Home",
+            "id": "home-link",
+            "url": get_url("GREAT_HOME")
         },
-      "custom": {
-        "title": "Your export journey",
-        "id": "custom-page-link",
-        "url": get_url("CUSTOM_PAGE")
+        "custom": {
+            "title": "Your export journey",
+            "id": "custom-page-link",
+            "url": get_url("CUSTOM_PAGE")
         },
-      "export_readiness": {
-        "id": "export-readiness-links",
-        "title": "Export readiness",
-        "items": [
-            {
-                "title": "I'm new to exporting",
-                "id": "export-readiness-new",
-                "url": get_url("EXPORTING_NEW")
-            },
-            {
-                "title": "I export occasionally",
-                "id": "export-readiness-occasional",
-                "url": get_url("EXPORTING_OCCASIONAL")
-            },
-            {
-                "title": "I'm a regular exporter",
-                "id": "export-readiness-regular",
-                "url": get_url("EXPORTING_REGULAR")
-            }]
+        "export_readiness": {
+            "id": "export-readiness-links",
+            "title": "Export readiness",
+            "items": [
+                {
+                    "title": "I'm new to exporting",
+                    "id": "export-readiness-new",
+                    "url": get_url("EXPORTING_NEW")
+                },
+                {
+                    "title": "I export occasionally",
+                    "id": "export-readiness-occasional",
+                    "url": get_url("EXPORTING_OCCASIONAL")
+                },
+                {
+                    "title": "I'm a regular exporter",
+                    "id": "export-readiness-regular",
+                    "url": get_url("EXPORTING_REGULAR")
+                }
+            ]
         },
-      "guidance": {
-        "id": "guidance-links",
-        "title": "Guidance",
-        "items": [
-            {
-                "title": "Market research",
-                "id": "guidance-market-research",
-                "url": get_url("GUIDANCE_MARKET_RESEARCH")
-            },
-            {
-                "title": "Customer insight",
-                "id": "guidance-customer-insight",
-                "url": get_url("GUIDANCE_CUSTOMER_INSIGHT")
-            },
-            {
-                "title": "Finance",
-                "id": "guidance-finance",
-                "url": get_url("GUIDANCE_FINANCE")
-            },
-            {
-                "title": "Business planning",
-                "id": "guidance-business-planning",
-                "url": get_url("GUIDANCE_BUSINESS_PLANNING")
-            },
-            {
-                "title": "Getting paid",
-                "id": "guidance-getting-paid",
-                "url": get_url("GUIDANCE_GETTING_PAID")
-            },
-            {
-                "title": "Operations and compliance",
-                "id": "guidance-operations-and-compliance",
-                "url": get_url("GUIDANCE_OPERATIONS_AND_COMPLIANCE")
-            }]
+        "guidance": {
+            "id": "guidance-links",
+            "title": "Guidance",
+            "items": [
+                {
+                    "title": "Market research",
+                    "id": "guidance-market-research",
+                    "url": get_url("GUIDANCE_MARKET_RESEARCH")
+                },
+                {
+                    "title": "Customer insight",
+                    "id": "guidance-customer-insight",
+                    "url": get_url("GUIDANCE_CUSTOMER_INSIGHT")
+                },
+                {
+                    "title": "Finance",
+                    "id": "guidance-finance",
+                    "url": get_url("GUIDANCE_FINANCE")
+                },
+                {
+                    "title": "Business planning",
+                    "id": "guidance-business-planning",
+                    "url": get_url("GUIDANCE_BUSINESS_PLANNING")
+                },
+                {
+                    "title": "Getting paid",
+                    "id": "guidance-getting-paid",
+                    "url": get_url("GUIDANCE_GETTING_PAID")
+                },
+                {
+                    "title": "Operations and compliance",
+                    "id": "guidance-operations-and-compliance",
+                    "url": get_url("GUIDANCE_OPERATIONS_AND_COMPLIANCE")
+                }]
         },
-      "services": {
-        "id": "services-links",
-        "title": "Services",
-        "items": [
-            {
-                "id": "services-find-a-buyer",
-                "title": "Create an export profile",
-                "url": get_url("SERVICES_FAB"),
-                "description": (
-                    "Get promoted internationally with a great.gov.uk "
-                    "trade profile")
-            },
-            {
-                "id": "services-selling-online-overseas",
-                "title": "Sell online overseas",
-                "url": get_url("SERVICES_SOO"),
-                "description": (
-                    "Find the right marketplace for your business "
-                    "and access special offers for sellers")
-            },
-            {
-                "id": "services-export-opportunities",
-                "title": "Find export opportunities",
-                "url": get_url("SERVICES_EXOPPS"),
-                "description": "Find and apply for overseas opportunities"
-            },
-            {
-                "id": "services-get-finance",
-                "title": "Get finance",
-                "url": get_url("SERVICES_GET_FINANCE"),
-                "description": "Get the finance you need to compete and grow"
-            },
-            {
-                "id": "services-events",
-                "title": "Find events and visits",
-                "url": get_url("SERVICES_EVENTS"),
-                "description": (
-                    "Attend events and see how visits by "
-                    "ministers can support your trade deals")
-            }]
+        "services": {
+            "id": "services-links",
+            "title": "Services",
+            "items": [
+                {
+                    "id": "services-find-a-buyer",
+                    "title": "Create an export profile",
+                    "url": get_url("SERVICES_FAB"),
+                    "description": (
+                        "Get promoted internationally with a great.gov.uk "
+                        "trade profile")
+                },
+                {
+                    "id": "services-selling-online-overseas",
+                    "title": "Sell online overseas",
+                    "url": get_url("SERVICES_SOO"),
+                    "description": (
+                        "Find the right marketplace for your business "
+                        "and access special offers for sellers")
+                },
+                {
+                    "id": "services-export-opportunities",
+                    "title": "Find export opportunities",
+                    "url": get_url("SERVICES_EXOPPS"),
+                    "description": "Find and apply for overseas opportunities"
+                },
+                {
+                    "id": "services-get-finance",
+                    "title": "Get finance",
+                    "url": get_url("SERVICES_GET_FINANCE"),
+                    "description": (
+                        "Get the finance you "
+                        "need to compete and grow")
+                },
+                {
+                    "id": "services-events",
+                    "title": "Find events and visits",
+                    "url": get_url("SERVICES_EVENTS"),
+                    "description": (
+                        "Attend events and see how visits by "
+                        "ministers can support your trade deals")
+                }]
         },
-      "site_links": {
-        "items": [
-            {
-                "id": "site-links-about",
-                "title": "About",
-                "url": get_url("INFO_ABOUT")
-            },
-            {
-                "id": "site-links-contact",
-                "title": "Contact us",
-                "url": get_url("INFO_CONTACT_US"),
-            },
-            {
-                "id": "site-links-privacy-and-cookies",
-                "title": "Privacy and cookies",
-                "url": get_url("INFO_PRIVACY_AND_COOKIES"),
-            },
-            {
-                "id": "site-links-t-and-c",
-                "title": "Terms and conditions",
-                "url": get_url("INFO_TERMS_AND_CONDITIONS"),
-            },
-            {
-                "id": "site-links-dit",
-                "title": "Department for International Trade on GOV.UK",
-                "url": get_url("INFO_DIT"),
-            }
-        ]}
+        "site_links": {
+            "items": [
+                {
+                    "id": "site-links-about",
+                    "title": "About",
+                    "url": get_url("INFO_ABOUT")
+                },
+                {
+                    "id": "site-links-contact",
+                    "title": "Contact us",
+                    "url": get_url("INFO_CONTACT_US"),
+                },
+                {
+                    "id": "site-links-privacy-and-cookies",
+                    "title": "Privacy and cookies",
+                    "url": get_url("INFO_PRIVACY_AND_COOKIES"),
+                },
+                {
+                    "id": "site-links-t-and-c",
+                    "title": "Terms and conditions",
+                    "url": get_url("INFO_TERMS_AND_CONDITIONS"),
+                },
+                {
+                    "id": "site-links-dit",
+                    "title": "Department for International Trade on GOV.UK",
+                    "url": get_url("INFO_DIT"),
+                }
+            ]}
     }
     return {
         'header_footer_links': header_footer_links
