1cf3852828fa07343d0a4804715450293aeb8dd3
eadpearce <e.pearce@tutanota.com>
2018-02-05 10:21:02 +0000

    Renamed context processor

--- demo/settings.py
+++ demo/settings.py
@@ -62,7 +62,7 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.template.context_processors.i18n',
-                'directory_components.context_processors.header_footer_urls',
+                'directory_components.context_processors.urls_processor',
             ],
         },
     },
