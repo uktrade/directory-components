35dee4bbd0e5b702d394b176a3de60e2c0a9a473
eadpearce <e.pearce@tutanota.com>
2018-02-20 11:23:49 +0000

    Add trailing slash

--- directory_components/urls.py
+++ directory_components/urls.py
@@ -4,7 +4,7 @@ HEADER_FOOTER_URLS_FAB = 'https://find-a-buyer.export.great.gov.uk/'
 HEADER_FOOTER_URLS_SOO = 'https://selling-online-overseas.export.great.gov.uk/'
 HEADER_FOOTER_URLS_EVENTS = 'https://events.trade.gov.uk/'
 HEADER_FOOTER_URLS_CONTACT_US = (
-    'https://contact-us.export.great.gov.uk/directory/FeedbackForm')
+    'https://contact-us.export.great.gov.uk/directory/FeedbackForm/')
 HEADER_FOOTER_URLS_DIT = (
     'https://www.gov.uk/government/organisations/'
     'department-for-international-trade/')
