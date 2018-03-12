eacad1fa1fccc2a2fe2d9c7e0f07b2c2a29f5301
richtier <rikatee@gmail.com>
2018-03-12 15:22:26 +0000

    Add social share features

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -12,3 +12,30 @@ from directory_components import helpers
 def test_add_next(target, expected):
     next_url = 'next.com'
     assert helpers.add_next(target, next_url) == expected
+
+
+def test_build_twitter_link(rf):
+    social_links_builder = helpers.SocialLinkBuilder(
+        url='http://testserver/',
+        page_title='Do research first',
+        app_title='Export Readiness',
+    )
+
+    assert social_links_builder.links['twitter'] == (
+        'https://twitter.com/intent/tweet'
+        '?text=Export%20Readiness%20-%20Do%20research%20first%20'
+        'http://testserver/'
+    )
+    assert social_links_builder.links['facebook'] == (
+        'https://www.facebook.com/share.php?u=http://testserver/'
+    )
+    assert social_links_builder.links['linkedin'] == (
+        'https://www.linkedin.com/shareArticle?mini=true&'
+        'url=http://testserver/&'
+        'title=Export%20Readiness%20-%20Do%20research%20first%20'
+        '&source=LinkedIn'
+    )
+    assert social_links_builder.links['email'] == (
+        'mailto:?body=http://testserver/'
+        '&subject=Export%20Readiness%20-%20Do%20research%20first%20'
+    )
