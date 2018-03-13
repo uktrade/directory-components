44741a6a1d57ae5808926c6e96410b6a85d7376d
eadpearce <e.pearce@tutanota.com>
2018-03-13 16:07:46 +0000

    Add widgets view

--- demo/urls.py
+++ demo/urls.py
@@ -13,4 +13,9 @@ urlpatterns = [
         views.NotFound.as_view(),
         name='404',
     ),
+    url(
+        r'^widgets/$',
+        views.demo_form,
+        name='widgets',
+    ),
 ]
--- demo/views.py
+++ demo/views.py
@@ -1,4 +1,7 @@
 from django.views.generic import TemplateView
+from .forms import DemoForm
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 
 class HelloWorld(TemplateView):
@@ -7,3 +10,8 @@ class HelloWorld(TemplateView):
 
 class NotFound(TemplateView):
     template_name = 'demo/404.html'
+
+
+def demo_form(request):
+    form = DemoForm()
+    return render(request, 'demo/widgets.html', {'form': form})
