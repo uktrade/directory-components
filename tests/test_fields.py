eb3d4dbea644a33606a8217416565495e42a45a3
richtier <rikatee@gmail.com>
2018-04-04 09:31:44 +0100

    Allow setting explicit css classes on widgets

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -1,3 +1,5 @@
+import pytest
+
 from django import forms
 
 from directory_components import fields
@@ -22,3 +24,19 @@ def test_padded_field_handles_empty():
 
         assert form.is_valid() is False
         assert form.errors['field'] == [REQUIRED_MESSAGE]
+
+
+@pytest.mark.parametrize('field_class', (
+    fields.CharField,
+    fields.URLField,
+    fields.BooleanField,
+))
+def test_explicit_widget_attrs(field_class):
+    field = field_class()
+
+    field_explicit_attrs = field_class(
+        widget=forms.TextInput(attrs={'class': 'a-class'})
+    )
+
+    assert field.widget.attrs['class'] == ' form-control'
+    assert field_explicit_attrs.widget.attrs['class'] == 'a-class form-control'
