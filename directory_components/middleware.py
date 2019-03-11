00c645d5390d92286556c7821dd17676b3ffc06c
eadpearce <e.pearce@tutanota.com>
2019-03-11 19:12:19 +0000

    Add functionality for country selector

--- directory_components/middleware.py
+++ directory_components/middleware.py
@@ -133,3 +133,25 @@ def is_path_resolvable(path):
         return False
     else:
         return True
+
+
+class CountryMiddleware:
+    def process_request(self, request):
+        country_code = helpers.get_country_from_querystring(request)
+        if country_code:
+            request.COUNTRY_CODE = country_code
+
+    def process_response(self, request, response):
+        """
+        Shares config with the language cookie as they serve a similar purpose
+        """
+
+        if hasattr(request, 'COUNTRY_CODE'):
+            response.set_cookie(
+                key=settings.COUNTRY_COOKIE_NAME,
+                value=request.COUNTRY_CODE,
+                max_age=settings.LANGUAGE_COOKIE_AGE,
+                path=settings.LANGUAGE_COOKIE_PATH,
+                domain=settings.LANGUAGE_COOKIE_DOMAIN
+            )
+        return response
---
+++ directory_components/mixins.py
@@ -0,0 +1,35 @@
+from directory_constants.constants.choices import COUNTRY_CHOICES
+
+from directory_components import helpers, forms
+
+
+class RegionalContentMixin:
+    country_form_class = forms.CountryForm
+
+    def get_context_data(self, *args, **kwargs):
+        country_code = helpers.get_user_country(self.request)
+
+        # if there is a country already detected we can hide the selector
+        hide_country_selector = bool(country_code)
+        country_name = dict(COUNTRY_CHOICES).get(country_code, '')
+
+        country = {
+            # used for flag icon css class. must be lowercase
+            'code': country_code.lower(),
+            'name': country_name,
+        }
+
+        country_form_kwargs = self.get_country_form_kwargs()
+
+        return super().get_context_data(
+            hide_country_selector=hide_country_selector,
+            country=country,
+            form=self.country_form_class(**country_form_kwargs),
+            *args, **kwargs
+        )
+
+    def get_country_form_kwargs(self, **kwargs):
+        return {
+            'initial': forms.get_country_form_initial_data(self.request),
+            **kwargs,
+        }
