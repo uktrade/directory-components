29a169becbe97ea0bfb8fcea70a984deafededdd
Kenneth Siu <kenneth.siu@softwire.com>
2019-04-02 14:32:27 +0100

    Update version number...

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='directory_components',
-    version='8.3.1',
+    version='8.3.2',
     url='https://github.com/uktrade/directory-components',
     license='MIT',
     author='Department for International Trade',
