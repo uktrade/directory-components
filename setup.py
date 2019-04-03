589f5624140afc9af3be83aeb5e5c8aa812545b5
Unknown <jessica.gilbert@softwire.com>
2019-04-03 14:27:12 +0100

    Changes wrong role attribute image to img

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='directory_components',
-    version='8.5.0',
+    version='8.6.0',
     url='https://github.com/uktrade/directory-components',
     license='MIT',
     author='Department for International Trade',
