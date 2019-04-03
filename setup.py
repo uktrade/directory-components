1933a110358cf79687af494de29696452374b4d9
Kenneth Siu <kenneth.siu@softwire.com>
2019-04-03 12:22:06 +0100

    Statistics can have number first

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='directory_components',
-    version='8.4.1',
+    version='8.5.0',
     url='https://github.com/uktrade/directory-components',
     license='MIT',
     author='Department for International Trade',
