54e5f2a57848d80f9bc26a9379bf4b6527be0f86
eadpearce <e.pearce@tutanota.com>
2019-04-11 14:20:32 +0100

    Fix tests

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -4,14 +4,14 @@ from directory_components import forms
 
 
 def test_get_language_form_initial_data():
-    translation.activate('fr')
-    request_stub = {}
-    data = forms.get_language_form_initial_data(request_stub)
-    assert data['language'] == 'fr'
+    with translation.override('fr'):
+        request_stub = {}
+        data = forms.get_language_form_initial_data(request_stub)
+        assert data['language'] == 'fr'
 
 
 def test_get_lang_form_initial_data():
-    translation.activate('fr')
-    request_stub = {}
-    data = forms.get_lang_form_initial_data(request_stub)
-    assert data['lang'] == 'fr'
+    with translation.override('fr'):
+        request_stub = {}
+        data = forms.get_lang_form_initial_data(request_stub)
+        assert data['lang'] == 'fr'
