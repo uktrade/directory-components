38d79436328422c5ae6c6c534d57b1459551c888
Mike Walker <Michael.Walker@softwire.com>
2019-05-08 13:22:02 +0100

    Fix adding tags to the international header.
    Delays the adding of tags until the document is ready, and fixes some links missing the GA attribute

--- directory_components/static/directory_components/js/dit.tagging.internationalHeader.js
+++ directory_components/static/directory_components/js/dit.tagging.internationalHeader.js
@@ -1,37 +1,39 @@
 dit.tagging.internationalHeader = new function() {
 
     this.init = function() {
-        $("[data-ga-class='banner-choose-country-form']").on("submit", function() {
-            window.dataLayer.push({
-                'eventAction': 'Select Country',
-                'eventLabel': 'International Header',
-                'eventValue': $("#great-header-country-select").val()
+        $(document).ready(function() {
+            $("[data-ga-class='banner-choose-country-form']").on("submit", function() {
+                window.dataLayer.push({
+                    'eventAction': 'Select Country',
+                    'eventLabel': 'International Header',
+                    'eventValue': $("#great-header-country-select").val()
+                });
             });
-        });
 
-        $("[data-ga-class='change-country-form']").on("submit", function() {
-            window.dataLayer.push({
-                'eventAction': 'Change Country',
-                'eventLabel': 'International Header',
-                'eventValue': $("#great-header-country-select").val()
+            $("[data-ga-class='change-country-form']").on("submit", function() {
+                window.dataLayer.push({
+                    'eventAction': 'Change Country',
+                    'eventLabel': 'International Header',
+                    'eventValue': $("#great-header-country-select").val()
+                });
             });
-        });
 
-        $("[data-ga-class='change-language-form']").on("submit", function() {
-            window.dataLayer.push({
-                'eventAction': 'Change Language',
-                'eventLabel': 'International Header',
-                'eventValue': $("#great-header-language-select").val()
+            $("[data-ga-class='change-language-form']").on("submit", function() {
+                window.dataLayer.push({
+                    'eventAction': 'Change Language',
+                    'eventLabel': 'International Header',
+                    'eventValue': $("#great-header-language-select").val()
+                });
             });
-        });
 
-        $("[data-ga-class='header-navigation']").on("click", function() {
-            window.dataLayer.push({
-                'eventAction': 'Navigation',
-                'eventCategory': 'Header Link',
-                'eventLabel': 'International Header',
-                'eventValue': $(this).text()
-            });
-        })
+            $("[data-ga-class='header-navigation']").on("click", function() {
+                window.dataLayer.push({
+                    'eventAction': 'Navigation',
+                    'eventCategory': 'Header Link',
+                    'eventLabel': 'International Header',
+                    'eventValue': $(this).text()
+                });
+            })
+        });
     };
 };
