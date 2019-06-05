a36cd1b0854fdd47b2b3a35bc03f29657633d39d
Kenneth Siu <kenneth.siu@softwire.com>
2019-06-05 16:29:33 +0100

    No-js version of mobile header

--- directory_components/static/directory_components/js/dit.components.greatInternationalHeader.js
+++ directory_components/static/directory_components/js/dit.components.greatInternationalHeader.js
@@ -5,26 +5,7 @@ dit.classes = dit.classes || {};
 dit.components = dit.components || {};
 dit.constants = dit.constants || {};
 
-dit.components.internationalHeader = (new function() {
-  const self = this;
-
-  function toggleSubNavMobile(subNavElementId) {
-    const subNavId = subNavElementId.split("-").pop();
-    const subNavList = document.getElementById("sub-nav-mobile-list-" + subNavId);
-    subNavList.hidden = !subNavList.hidden;
-  }
-
-  self.init = function() {
-    const subNavMobileToggles = document.getElementsByClassName("sub-nav-mobile-toggle");
-    for (let i = 0; i < subNavMobileToggles.length; i++) {
-      const subNavMobileToggle = subNavMobileToggles[i];
-      subNavMobileToggle.onclick = function() { toggleSubNavMobile(subNavMobileToggle.id); };
-    }
-  };
-});
-
 $(document).ready(function() {
   dit.components.countrySelector.init();
   dit.components.languageSelectorDropdown.init();
-  dit.components.internationalHeader.init();
 });
