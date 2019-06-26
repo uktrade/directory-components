d5cace83d35468c13a120325d7dad1245a6d6f1e
Kenneth Siu <kenneth.siu@softwire.com>
2019-06-26 13:00:33 +0100

    Move to getting header items from context processor

--- directory_components/mixins.py
+++ directory_components/mixins.py
@@ -1,3 +1,7 @@
+from directory_components.context_processors import (
+    header_footer_processor,
+    urls_processor
+)
 from django.conf import settings
 from django.utils import translation
 
@@ -125,8 +129,159 @@ class GA360Mixin:
 
 class InternationalHeaderMixin:
     def get_context_data(self, *args, **kwargs):
+
+        header_footer_urls = header_footer_processor(None)['header_footer_urls']  # noqa
+        services_urls = urls_processor(None)['services_urls']
+
+        header_items = [
+            {
+                'name': 'about_uk',
+                'title': 'About the UK',
+                'url': 'https://great.uat.uktrade.io/international/the-uk/',  # noqa
+                'sub_pages': [
+                    {
+                        'name': 'about_uk_home',
+                        'title': 'Overview',
+                        'url': 'https://great.uat.uktrade.io/international/the-uk/'  # noqa
+                    },
+                    {
+                        'name': 'industries',
+                        'title': 'Industries',
+                        'url': header_footer_urls['industries']
+                    },
+                    {
+                        'name': 'regions',
+                        'title': 'Regions',
+                        'url': 'https://great.uat.uktrade.io/international/the-uk/regions/'  # noqa
+                    }
+                ]
+            },
+            {
+                'name': 'expand',
+                'title': 'Start a company',
+                'url': services_urls['invest'],
+                'sub_pages': [
+                    {
+                        'name': 'expand_home',
+                        'title': 'Overview',
+                        'url': services_urls['invest']
+                    },
+                    {
+                        'name': 'high_potential_opportunities',
+                        'title': 'High-potential opportunities',
+                        'url': services_urls['invest'] + '#high-potential-opportunities'  # noqa
+                    },
+                    {
+                        'name': 'uk_setup_guide',
+                        'title': 'How to set up a company',
+                        'url': services_urls['uk_setup_guide']
+                    },
+                    {
+                        'name': 'isd',
+                        'title': 'Professional advice',
+                        'url': services_urls['isd']
+                    }
+                ]
+            },
+            {
+                'name': 'capital_invest',
+                'title': 'Invest capital',
+                'url': services_urls['capital_invest'],
+                'sub_pages': [
+                    {
+                        'name': 'capital_invest_home',
+                        'title': 'Overview',
+                        'url': services_urls['capital_invest']
+                    },
+                    {
+                        'name': 'investment_types',
+                        'title': 'Investment types',
+                        'url': 'https://great.uat.uktrade.io/international/invest-capital/types/'  # noqa
+                    },
+                    {
+                        'name': 'investment_opportunities',
+                        'title': 'Investment opportunities',
+                        'url': 'https://great.uat.uktrade.io/international/invest-capital/opportunities/'  # noqa
+                    },
+                    {
+                        'name': 'investment_guides',
+                        'title': 'How to invest capital',
+                        'url': 'https://great.uat.uktrade.io/international/invest-capital/guides/'  # noqa
+                    },
+                    {
+                        'name': 'isd',
+                        'title': 'Professional advice',
+                        'url': services_urls['isd']
+                    }
+                ]
+            },
+            {
+                'name': 'find_a_supplier',
+                'title': 'Buy from the UK',
+                'url': header_footer_urls['fas'],
+                'sub_pages': [
+                    {
+                        'name': 'find_a_supplier_home',
+                        'title': 'Overview',
+                        'url': header_footer_urls['fas']
+                    },
+                    {
+                        'name': 'import_guides',
+                        'title': 'How to buy from the UK',
+                        'url': 'https://great.uat.uktrade.io/international/import/guides/'  # noqa
+                    },
+                    {
+                        'name': 'supplier_directory',
+                        'title': 'Supplier directory',
+                        'url': header_footer_urls['fas'] + '?q=&sectors='
+                    }
+                ]
+            },
+            {
+                'name': 'about_dit',
+                'title': 'About us',
+                'url': 'https://great.uat.uktrade.io/international/about-dit/',
+                'sub_pages': [
+                    {
+                        'name': 'about_dit_home',
+                        'title': 'What we do',
+                        'url': 'https://great.uat.uktrade.io/international/about-dit/'  # noqa
+                    },
+                    {
+                        'name': 'dit_case_studies',
+                        'title': 'Success stories',
+                        'url': 'https://great.uat.uktrade.io/international/about-dit/case-studies/'  # noqa
+                    },
+                    {
+                        'name': 'contact_us',
+                        'title': 'Contact us',
+                        'url': services_urls['contact_us']
+                    }
+                ]
+            },
+            {
+                'name': 'news_and_events',
+                'title': 'News and events',
+                'url': 'https://great.uat.uktrade.io/international/latest/',
+                'sub_pages': [
+                    {
+                        'name': 'news',
+                        'title': 'News',
+                        'url': 'https://www.gov.uk/government/organisations/department-for-international-trade'  # noqa
+                    },
+                    {
+                        'name': 'events',
+                        'title': 'Events',
+                        'url': services_urls['events']
+                    }
+                ]
+            }
+        ]
+
         return super().get_context_data(
             international_header_area=self.international_header_area,
+            international_header_selected_page=self.international_header_selected_page,  # noqa
+            header_items=header_items,
             *args,
             **kwargs
         )
