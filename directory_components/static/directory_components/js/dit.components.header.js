81e14deba92b3cd0d237b7b9341aa98d19197650
Mike <Walker>
2019-07-24 12:18:44 +0100

    Add arrow controls for the mobile menu.

--- directory_components/static/directory_components/js/dit.components.header.js
+++ directory_components/static/directory_components/js/dit.components.header.js
@@ -1,12 +1,20 @@
-
 var dit = dit || {};
-
-dit.classes = dit.classes || {};
 dit.components = dit.components || {};
-dit.constants = dit.constants || {};
 
 dit.components.header = (new function() {
   var self = this;
+
+  self.MENU_BUTTON = "#mobile-menu-button";
+  self.MOBILE_NAV = "#great-header-mobile-nav";
+  self.SEARCH_WRAPPER = "#great-header-search-wrapper";
+  self.MENU_ITEMS_TAG = "js-menu-index";
+  self.SUB_MENU_ITEMS_TAG = "js-sub-menu-index";
+  self.MENU_ITEM_CONTAINER = "[data-js-menu-item-container]";
+  self.SUB_MENU = "[data-js-sub-menu]";
+
+  self.dataAttributeSelector = function(dataAttributeName) {
+    return "[data-" + dataAttributeName + "]";
+  };
 
   self.showButton = function() {
     $('#mobile-menu-button').addClass('ready');
@@ -24,33 +32,156 @@ dit.components.header = (new function() {
   };
 
   self.openMenu = function() {
-    $('#mobile-menu-button').addClass('expanded').attr('aria-expanded', 'true');
-    $('#great-header-mobile-nav').addClass('expanded').attr('aria-expanded', 'true');
-    $('#great-header-search-wrapper').hide();
+    $(self.MENU_BUTTON).addClass('expanded').attr('aria-expanded', 'true');
+    $(self.MOBILE_NAV).addClass('expanded').attr('aria-expanded', 'true');
+    $(self.SEARCH_WRAPPER).hide();
+
+    self.moveFocusToMenuButton();
   };
 
   self.closeMenu = function() {
-    var menuButton = $('#mobile-menu-button');
-    menuButton.removeClass('expanded').attr('aria-expanded', 'false');
-    $('#great-header-mobile-nav').removeClass('expanded').attr('aria-expanded', 'false');
-    $('#great-header-search-wrapper').show();
+    $(self.MENU_BUTTON).removeClass('expanded').attr('aria-expanded', 'false');
+    $(self.MOBILE_NAV).removeClass('expanded').attr('aria-expanded', 'false');
+    $(self.SEARCH_WRAPPER).show();
 
-    menuButton.focus();
+    self.moveFocusToMenuButton();
+  };
+
+  self.moveFocusToMenuButton = function() {
+    $(self.MENU_BUTTON).focus();
+  };
+
+  self.getMenuItem = function(tag, index) {
+    return $('[data-' + tag + '=' + index + ']');
+  };
+
+  self.getSubMenuItem = function(container, tag, index) {
+    return container.find('[data-' + tag + '=' + index + ']').first();
+  };
+
+  self.moveFocusToFirstMenuItem = function() {
+    self.getMenuItem(self.MENU_ITEMS_TAG, 0).focus();
+  };
+  
+  self.moveFocusToSubMenu = function(target) {
+    var mainMenuItemContainer = $(target).closest(self.MENU_ITEM_CONTAINER);
+    mainMenuItemContainer.find(self.dataAttributeSelector(self.SUB_MENU_ITEMS_TAG)).first().focus();
+  };
+
+  self.moveFocusToMainMenu = function(target) {
+    var mainMenuItemContainer = $(target).closest(self.MENU_ITEM_CONTAINER);
+    var mainMenuItem = mainMenuItemContainer.find(self.dataAttributeSelector(self.MENU_ITEMS_TAG)).first();
+    mainMenuItem.focus();
+    return mainMenuItem;
+  };
+
+  self.moveFocusToPreviousMenuItem = function(target) {
+    var currentIndex = parseInt($(target).data(self.MENU_ITEMS_TAG));
+    if (currentIndex === 0) {
+      self.closeMenu()
+    } else {
+      self.getMenuItem(self.MENU_ITEMS_TAG, currentIndex - 1).focus();
+    }
+  };
+  
+  self.moveFocusToPreviousSubMenuItem = function(target) {
+    var currentIndex = parseInt($(target).data(self.SUB_MENU_ITEMS_TAG));
+    if (currentIndex === 0) {
+      self.moveFocusToMainMenu(target);
+    } else {
+      var subMenu = $(target).closest(self.SUB_MENU);
+      self.getSubMenuItem(subMenu, self.SUB_MENU_ITEMS_TAG, currentIndex - 1).focus();
+    }
+  };
+
+  self.moveFocusToNextMenuItem = function(target) {
+    var currentIndex = parseInt($(target).data(self.MENU_ITEMS_TAG));
+    var nextItem = self.getMenuItem(self.MENU_ITEMS_TAG, currentIndex + 1);
+    if (nextItem.length) {
+      nextItem.focus();
+    } else {
+      // at the end of the menu, loop back to the top
+     self.moveFocusToFirstMenuItem();
+    }
+  };
+  
+  self.moveFocusToNextSubMenuItem = function(target) {
+    var currentIndex = parseInt($(target).data(self.SUB_MENU_ITEMS_TAG));
+    var subMenu = $(target).closest(self.SUB_MENU);
+    var nextItem = self.getSubMenuItem(subMenu, self.SUB_MENU_ITEMS_TAG, currentIndex + 1);
+    if (nextItem.length) {
+      nextItem.focus();
+    } else {
+      // at the end of the sub menu, return to the next item in the main menu.
+      var mainMenuItem = self.moveFocusToMainMenu(target);
+      self.moveFocusToNextMenuItem(mainMenuItem);
+    }
+  };
+
+  self.handleMenuButtonKeyDownEvents = function(event) {
+      if (event.key === "Escape" || event.key === "ArrowUp") {
+        self.closeMenu();
+        event.preventDefault();
+      }
+      if (event.key === "ArrowDown") {
+        self.openMenu();
+        self.moveFocusToFirstMenuItem();
+        event.preventDefault();
+      }
+  };
+
+  self.handleMenuItemKeyDownEvents = function(event) {
+    if (event.key === "Escape") {
+      self.closeMenu();
+      event.preventDefault();
+    }
+    if (event.key === "ArrowUp") {
+      self.moveFocusToPreviousMenuItem(event.target);
+      event.preventDefault();
+    }
+    if (event.key === "ArrowDown") {
+      self.moveFocusToNextMenuItem(event.target);
+      event.preventDefault();
+    }
+    if (event.key === "ArrowRight") {
+      self.moveFocusToSubMenu(event.target);
+      event.preventDefault();
+    }
+  };
+  
+  self.handleSubMenuItemKeyDownEvents = function(event) {
+    if (event.key === "Escape") {
+      self.closeMenu();
+      event.preventDefault();
+    }
+    if (event.key === "ArrowUp") {
+      self.moveFocusToPreviousSubMenuItem(event.target);
+      event.preventDefault();
+    }
+    if (event.key === "ArrowDown") {
+      self.moveFocusToNextSubMenuItem(event.target);
+      event.preventDefault();
+    }
+    if (event.key === "ArrowLeft") {
+      self.moveFocusToMainMenu(event.target);
+      event.preventDefault();
+    }
+  };
+
+  self.setupEventListeners = function() {
+    $(self.MENU_BUTTON)
+        .on("click", self.toggleMenu)
+        .on("keydown", self.handleMenuButtonKeyDownEvents);
+
+    $(self.dataAttributeSelector(self.MENU_ITEMS_TAG))
+        .on("keydown", self.handleMenuItemKeyDownEvents);
+
+    $(self.dataAttributeSelector(self.SUB_MENU_ITEMS_TAG))
+        .on("keydown", self.handleSubMenuItemKeyDownEvents);
   };
 
   self.init = function() {
-    var mobileMenuButton = document.getElementById("mobile-menu-button");
-    var header = document.getElementById("great-header-menu");
-
-    mobileMenuButton.addEventListener("click", function () {
-      self.toggleMenu();
-    });
-
-    header.addEventListener("keyup", function(event) {
-      if (event.key === "Escape") {
-        self.closeMenu();
-      }
-    });
+    self.setupEventListeners();
 
     // menu should start closed
     self.closeMenu();
