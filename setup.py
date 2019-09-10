2c81b1eb9396b9fbed7e01554df247b5727992d5
eadpearce <e.pearce@tutanota.com>
2019-09-10 15:49:27 +0100

    Update version, changelog

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='directory_components',
-    version='32.0.1',
+    version='32.0.2',
     url='https://github.com/uktrade/directory-components',
     license='MIT',
     author='Department for International Trade',
