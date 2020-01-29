7f173d6a65f7511b95eb071010837ef13dcf7ba2
Will Taylor <wrftaylor@gmail.com>
2020-01-29 16:49:44 +0000

    MVP - fix linting issue

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -4,12 +4,12 @@ from directory_components.decorators import skip_ga360
 
 
 @skip_ga360
-def test_function(request, *args, **kwargs):
+def get_response(request, *args, **kwargs):
     return HttpResponse()
 
 
 def test_skip_360_adds_attribute_to_response():
     request = HttpRequest()
-    response = test_function(request)
+    get_response(request)
 
     assert request.skip_ga360 is True
