392406fb5a5b9c29cc582f366ba4313181af01b6
Jeanette Murphy <jeanettemurphy@meggaweb.co.uk>
2020-01-30 11:10:43 +0000

    changelog and setup

--- directory_components/static/directory_components/js/dit.components.cookie-notice.js
+++ directory_components/static/directory_components/js/dit.components.cookie-notice.js
@@ -4,6 +4,7 @@ dit.components = dit.components || {};
 dit.components.cookieNotice = function() {
   var COOKIE_NOTICE_ID = 'header-cookie-notice';
   var COOKIE_CLOSE_BUTTON_ID = 'dismiss-cookie-notice';
+  var COOKIE_DOMAIN = $('#privacyCookieDomain').attr('value');
   var cookiePreferencesName = 'cookie_preferences_set';
   var cookiePreferencesDurationDays = 365;
   var cookiesPolicyName = 'cookies_policy';
@@ -13,7 +14,7 @@ dit.components.cookieNotice = function() {
     if (typeof options === 'undefined') {
       options = {};
     }
-    var cookieString = name + '=' + value + '; path=/';
+    var cookieString = name + '=' + value + '; domain=' + COOKIE_DOMAIN + ';path=/;';
     if (options.days) {
       var date = new Date();
       date.setTime(date.getTime() + options.days * 24 * 60 * 60 * 1000);
@@ -123,19 +124,6 @@ dit.components.cookieNotice = function() {
       createPoliciesCookie(true, true, true);
       setPreferencesCookie();
       displayCookieBannerAcceptAll(bannerClassName);
-
-      window.dataLayer.push({
-        'event': 'allowCookiePreferences',
-        'value': [
-          {
-          'usage': true,
-          },
-        {
-          'settings': true
-        }
-      ]
-      });
-
       return false;
     });
 
@@ -178,101 +166,3 @@ dit.components.cookieNotice = function() {
 window.onload = function() {
   dit.components.cookieNotice.init('.cookie-notice', '.button-accept', 'cookies');
 }
-
-// dit.components.cookieNotice = (new function() {
-
-//   var COOKIE_NOTICE_ID = 'header-cookie-notice';
-//   var COOKIE_CLOSE_BUTTON_ID = 'dismiss-cookie-notice';
-//   var COOKIE_DOMAIN = $('#privacyCookieDomain').attr('value');
-
-//   function viewInhibitor(activate) {
-//     var rule = '#header-cookie-notice { display: none; }';
-//     var style;
-//     if (arguments.length && activate) {
-//       style = document.createElement('style');
-//       style.setAttribute('type', 'text/css');
-//       style.setAttribute('id', 'cookie-notice-view-inhibitor');
-//       style.appendChild(document.createTextNode(rule));
-//       document.head.appendChild(style);
-//     }
-//     else {
-//       document.head.removeChild(document.getElementById('cookie-notice-view-inhibitor'));
-//     }
-//   };
-
-//   // Hide on load
-//   viewInhibitor(true);
-
-//   setCookie = function(name, value, days) {
-//     if (days) {
-//       var date = new Date();
-//       date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-//       value += '; expires=' + date.toGMTString();
-//     }
-//     document.cookie = name + '=' + value + '; domain=' + COOKIE_DOMAIN + ';path=/;';
-//   }
-
-//   getCookie = function(name) {
-//     if (document.cookie.length > 0) {
-//       var start = document.cookie.indexOf(name + '=');
-//       if (start !== -1) {
-//         start = start + name.length + 1;
-//         var end = document.cookie.indexOf(';', start);
-//         if (end == -1) {
-//           end = document.cookie.length;
-//         }
-//         return unescape(document.cookie.substring(start, end));
-//       }
-//     }
-//     return '';
-//   }
-
-//   hideNoticeAndSetCookie = function() {
-//     $('#header-cookie-notice').hide();
-//     setCookie('hide_cookie_notice', true, 30);
-//   }
-
-//   createCloseButton = function() {
-//     var $container = $('.cookie-notice-container');
-//     var $closeButton = $('<button>', {
-//       'class': 'cookie-close',
-//       'aria-controls': COOKIE_NOTICE_ID,
-//       'aria-label': 'Close this message',
-//       id: COOKIE_CLOSE_BUTTON_ID
-//     });
-//     $container.append($closeButton);
-//     return $closeButton;
-//   }
-
-//   closeButtonEventHandler = function() {
-//     var $button = createCloseButton();
-
-//     $button.on('keydown', function(e) {
-//       // Close on enter or space
-//       if(e.which === 13 || e.which === 32) {
-//         hideNoticeAndSetCookie();
-//       }
-//     });
-
-//     $button.on('click', function(e) {
-//       hideNoticeAndSetCookie();
-//       e.preventDefault();
-//     });
-//   }
-
-//   this.init = function() {
-//     closeButtonEventHandler();
-//     var showCookieNotice = !getCookie('hide_cookie_notice');
-//     if (showCookieNotice) {
-//       $('#header-cookie-notice').show();
-//     } else {
-//       $('#header-cookie-notice').hide();
-//     }
-//     viewInhibitor(false);
-//   }
-
-// });
-
-// dit.components.cookieNotice = cookieNotice;
-
-
