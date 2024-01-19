42faab68dec9b7c443736822260f1b2ec2dcb693
davidu1975 <david.urquhart@digital.trade.gov.uk>
2024-01-19 11:03:14 +0000

    update django 4.2.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='directory_components',
-    version='40.0.0',
+    version='40.1.0',
     url='https://github.com/uktrade/directory-components',
     license='MIT',
     author='Department for Business and Trade',
